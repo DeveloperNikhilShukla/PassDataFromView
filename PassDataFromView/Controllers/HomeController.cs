ed8de9f48e0eda99d7392c39299ec039c8316814
The Epic Hero <63442799+TheIndianHacker-crypto@users.noreply.github.com>
2020-04-13 03:58:48 +0530

    Initial commit

---
+++ PassDataFromView/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PassDataFromView.Models;
+using PassDataFromView.Controllers;
+
+namespace PassDataFromView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ReciveDataUsingStrongBind(StudentInformation _StudentInformation)
+        {
+            ViewData["myEntry"] = "your name is ='" + _StudentInformation.name + "'  your collage is ='" + _StudentInformation.Collage + "'   your age is ='" + _StudentInformation.age + "'";
+            TempData["myEntry"] = ViewData["myEntry"];
+            return View("index");
+        }
+
+
+        [HttpPost]
+        public ActionResult ReciveDataUsingParameaters(string name,string collage,string age)
+        {
+            ViewData["myEntry"] = "your name is ='" + name + "'  your collage is ='" + collage + "'   your age is ='" + age + "'";
+            TempData["myEntry"] = ViewData["myEntry"];
+            return View("index");
+        }
+
+
+        [HttpPost]
+        public ActionResult PostDataUsingRequest()
+        {
+
+            string name= Request["name"];
+            string collage = Request["collage"];
+            string age = Request["age"];
+
+            ViewData["myEntry"] = "your name is ='" + name + "'  your collage is ='" + collage + "'   your age is ='" + age + "'";
+            TempData["myEntry"] = ViewData["myEntry"];
+
+            return View("index");
+        }
+
+
+        [HttpPost]
+        public ActionResult postdatausingformcollection(FormCollection from)
+        {
+
+            string name = from["name"];
+            string collage = from["collage"];
+            string age = from["age"];
+
+            ViewData["myEntry"] = "your name is ='" + name + "'  your collage is ='" + collage + "'   your age is ='" + age + "'";
+            TempData["myEntry"] = ViewData["myEntry"];
+
+            return View("index");
+        }
+
+
+        public ActionResult postdatausinggetmethd(FormCollection from)
+        {
+
+            string name = from["name"];
+            string collage = from["collage"];
+            string age = from["age"];
+
+            ViewData["myEntry"] = "your name is ='" + name + "'  your collage is ='" + collage + "'   your age is ='" + age + "'";
+            TempData["myEntry"] = ViewData["myEntry"];
+
+            return View("index");
+        }
+
+    }
+}
---
+++ PassDataFromView/Models/StudentInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace PassDataFromView.Models
+{
+    public class StudentInformation
+    {
+
+        public string name { get; set; }
+        public string Collage { get; set; }
+        public string age { get; set; }
+    }
+}
